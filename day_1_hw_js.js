0155c4f6945d05ea2c27e2977bee7026ac677d09
EuanRamsay <euancramsay@aol.com>
2016-11-29 09:20:06 +0000

    Initial commit

---
+++ day_1_hw_js.js
@@ -0,0 +1,172 @@
+//Section 1
+
+//what types are these? Write your answer in a comment beside it.
+
+1; // number
+"cat"; // string
+true; // boolean
+[]; // array
+{}; // hash
+1.1; // number
+undefined; //undefined
+
+//Section 2
+
+// what is the truthy/falsiness values of the following
+// write your answer in a comment beside it
+// you can use an if to test this...
+1; //true
+"cat"; // true
+true; // true
+NaN; // true
+[]; // true
+{}; // true
+undefined; // false
+""; // false
+0; // false
+
+//Section 3
+
+//Using examples that are different from above...
+
+//3.1 Assign a variable that is a number
+
+var myNumber = 1;
+
+//3.2 Assign a variable that is a string
+
+var myString = "Yellow Submarine";
+
+//3.3 Assign a variable that is a boolean
+
+var myBoolean = false;
+
+//3.4 Assign a variable that is an object
+
+var myObject = {
+  name: "John",
+  age: 50,
+};
+
+//Section 4
+//4.1 Write a statement that writes "hello" to the console if it's true and "bye" if it is false
+
+if(true){
+  console.log( "hello");
+} else {
+  console.log ( "bye");
+}
+
+//Section 5
+var animals = ["raccoon","hedgehog","mouse","gerbil"];
+
+//5.1. Assign the first element to a variable
+
+var first = animals[0];
+
+//5.2. Assign the last element to a variable
+
+var last = animals.pop();
+
+//5.3. Assign the length of an array to a variable
+
+var arrayLength = animals.length;
+
+//5.4. Add an item to the end of the array
+
+animals.push( "rat" );
+
+//5.5. Add an item to the start of the array
+
+animals.unshift( "possum" );
+
+//5.6. Assign the index of hedgehog to a variable
+
+var hedgehogIndex = animals.indexOf("hedgehog");
+
+//Section 6
+
+//6.1 Create an array of 5 vegetables
+
+var vegetables = ["carrot", "leek", "kumara", "parsnip", "spinach"];
+
+//6.2 Loop over the array and write to the console using a "while"
+
+var x = 0;
+
+while (x < vegetables.length) {
+  console.log(vegetables[x]);
+  x++;
+};
+
+//6.3 Loop again using a "for" with a counter
+
+for (var i = 0; i < vegetables.length; i++) {
+  console.log(vegetables[i]);
+}
+
+//6.4 Loop again using a "for of"
+
+for (var veg of vegetables) {
+  console.log(veg);
+}
+
+//Section 7
+var accounts = [
+  { name: 'jay',
+    amount: 125.50,
+    type: 'personal'
+  },
+  { name: 'val',
+    amount: 55125.10,
+    type: 'business'
+  },
+  { name: 'marc',
+    amount: 400.00,
+    type: 'personal'
+  },
+  { name: 'keith',
+    amount: 220.25,
+    type: 'business'
+  },
+  { name: 'rick',
+    amount: 1.00,
+    type: 'personal'
+  },
+];
+//7.1 Calculate the total cash in accounts
+
+var total = 0;
+
+for (var account of accounts) {
+  for (var amount in account) {
+    var value = account.amount;
+    total += value;
+  }
+};
+
+//7.2 Find the amount of money in the account with the largest balance
+
+var highest = 0;
+
+for (var account of accounts) {
+  for (var amount in account) {
+    if (highest < account.amount){
+      highest = account.amount;
+    }
+  }
+};
+
+//7.3 Find the name of the account with the smallest balance
+
+
+
+//7.4 Calculate the average bank account value
+//7.5 Find the value of marcs bank account
+//7.6 Find the holder of the largest bank account
+//7.7 Calculate the total cash in business accounts
+//7.8 Find the largest personal account owner
+
+
+//Section 8
+//Assign a variable myPerson to a hash, giving them a name, height, favourite food and an eat method
